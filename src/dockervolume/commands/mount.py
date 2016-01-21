23b44aec452cae3317ce846b881e15a1843ee615
TakesxiSximada <takesxi.sximada@gmail.com>
2016-01-21 20:25:21 +0900

    single volume support

--- src/dockervolume/commands/mount.py
+++ src/dockervolume/commands/mount.py
@@ -13,19 +13,21 @@ from dockervolume.utils import (
 
 def main(argv=sys.argv[1:]):
     parser = argparse.ArgumentParser()
+    parser.add_argument('volumes', nargs='*')
     parser.add_argument('-m', '--machine', default=DEFAULT_MACHINE_NAME)
     parser.add_argument('--conf', default=DEFAULT_CONF)
     parser.add_argument('--dry-run', default=False, action='store_true')
     args = parser.parse_args(argv)
 
     machine = args.machine
+    volumes = args.volumes
     conf = search_docker_voluem_yml(args.conf)
     dry_run = args.dry_run
 
     setup_logging()
     registry = bootstrap(name=machine, conf=conf, dry_run=dry_run)
     execute = VolumeCommand(registry)
-    execute('mount')
+    execute('mount', *volumes)
 
 if __name__ == '__main__':
     sys.exit(main())
--- src/dockervolume/core.py
+++ src/dockervolume/core.py
@@ -9,9 +9,9 @@ class VolumeCommand(object):
     def __init__(self, registry):
         self.registry = registry
 
-    def __call__(self, cmd):
+    def __call__(self, cmd, *args, **kwds):
         func = self.registry.queryUtility(IOperation, cmd)
         if func is None:
             raise NoOperationError('`{}` is not supported command'.format(cmd))
         setting = self.registry.queryUtility(ISetting)
-        return func(setting)
+        return func(setting, *args, **kwds)
